59400c9f78dbfaecc940adf6659c36a95fb6edc5
AKI <a.okada3@gmail.com>
2017-08-02 10:45:34 -0700

    Create my First Lab

---
+++ Labs/lab1.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+
+int main(){
+
+//1.Decimal to Binary
+//1100
+//10111001
+//100100110
+
+//2.Binary to Decimal
+//45
+//235
+//10
+
+//3.Decimal to Hex
+//0x8a
+//0x77
+//0x3b
+
+//4.Hex to Decimal
+//26
+//16177
+//508
+
+//5.Binary to Hex
+//97
+//167F
+// 7
+
+//6.Hex to Binary
+//1101000111011
+//1010101111001101
+//100010001000100010001
+
+
+
+//<c>1.
+int int1 =5;
+int int2 =7;
+int sum = int1 + int2;
+printf("<int1>plus<int2>equals<");
+printf("%d\n", sum);
+printf(">");
+
+//2. gcc -o first.c lab1.c
+
+//3.
+
+//for(int i = 0; i < 5; i++){
+//printf(int1, int2);
+//}
+
+//4.
+
+//int  ---  4 bytes
+//Char   ---  1 byte
+//float --- 4 bytes
+//double --- 8 bytes
+//shorts --- 4 bytes/ 2 bytes
+//long --- 4 bytes/ 8 bytes
+
+
+}
+
