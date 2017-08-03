3c406df4b743dc74fa35676f614ab1d9800b94dd
AKI <a.okada3@gmail.com>
2017-08-03 11:46:36 -0700

    add Lab

---
+++ Labs/Lab2.c
@@ -0,0 +1,19 @@
+//1
+//The extra step we take is adding 1 to our complemented(flipping bites) number.
+//A good way of remember this is to comprement the name of the representation schemes. 
+//2. 1. 10111
+//   2. 10101
+//   3. 11001
+//   4. 11111
+//3. In 2's complement, all the positive numbers begin with a 0.
+//4. In 2's complement we have only ine way to represent 0. This simplifies our representaiton schemes and have major advantage for designing hardwares.
+
+//5. change to binary number 
+//6. make binary number that is 7 and flip the bits and add 1
+//7. If the number is positive just change to binary number. If not, change the decimal number to positive and change to binary number and flip the bits then add 1.
+//8. 1. 110000
+//   2. 001101
+//   3. 111101
+//   4. 110110
+//   5. 011010
+//   6. 100001
