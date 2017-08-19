407014894bcebaa4ada9e24c6d4008638b3f6999
AKI <a.okada3@gmail.com>
2017-08-18 17:54:31 -0700

    up

--- Project/draw1a.c
+++ Project/draw1a.c
@@ -10,46 +10,27 @@ fflush(stdout);
 system("date");
 FILE *fp;
 fp = fopen("inputfile.1", "r");
-printf("Input file : ./inputfile.1\n");
+printf("Input file : %s\n", path);
 char buffer[256];
-int image_count;
-int end_count;
-int lineto_count;
-int print_count;
-int draw_count;
-int translate_count;
-int child_count;
-int com_count;
-    while(!feof(fp)){
-        fscanf(fp,"%s%*[^\n]",buffer);
-        if(strcmp(buffer,"Image") == 0){
-            image_count++;
-        }else if(strcmp(buffer, "lineTo") == 0){
-            lineto_count++;
-        }else if(strcmp(buffer, "End") == 0){
-            end_count++;
-        }else if(strcmp(buffer, "print") == 0){
-            print_count++;
-        }else if(strcmp(buffer, "draw") == 0){
-            draw_count++;
-        }else if(strcmp(buffer, "translate") == 0){
-            translate_count++;
-        }else if(strcmp(buffer, "child") == 0){
-            child_count++;
-        }else if(strcmp(buffer, "#") == 0){
-            com_count++;
+const char *const keywords[] = {"Image", "lineTo", "End", "print", "draw", "translate", "child", "#"};
+int count[8] = {0};  
+while(!feof(fp)){
+    fscanf(fp,"%s%*[^\n]",buffer);
+    for(int i = 0; i < 8; i++) {
+        if (strcmp(keywords[i], buffer) == 0){
+            count[i]++;
         }
-
     }
+}
+for(int j = 0; j < 8; j++) {
+    if (j == 1) { 
+        printf("%d %s command(s) within Figures\n", count[j], keywords[j]);
+    }else if (j == 7) { 
+        printf("%d comment(s)\n", count[j]); 
+    }else { printf("%d %s command(s)\n", count[j], keywords[j]); 
+    }
+}
 
-printf("%d Image definition(s)\n", image_count);
-printf("%d lineto command(s)\n", lineto_count);
-printf("%d End command(s)\n", end_count);
-printf("%d print command(s)\n", print_count);
-printf("%d draw command(s)\n", draw_count);
-printf("%d translate command(s)\n", translate_count);
-printf("%d child command(s)\n", child_count);
-printf("%d comment(s)\n", com_count);
 fclose(fp);
 return 0;
 }
